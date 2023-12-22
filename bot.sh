2bf4b3158d12564150c56fc1e147fa86921ad486
Subidit Win <subidit@users.noreply.github.com>
2023-12-22 22:38:45 +0600

    changes

--- bot.sh
+++ bot.sh
@@ -4,7 +4,7 @@ START_Y=2012
 END_Y=2013
 
 for Y in $(eval "echo {$START_Y..$END_Y}"); do
-    for M in {01..12}; do
+    for M in {01..2}; do
         for D in {01..31}; do
             for i in {01..12}; do
                 GIT_AUTHOR_DATE="$Y-$M-$D $i:00:00" GIT_COMMITTER_DATE="$Y-$M-$D $i:00:00" git commit --allow-empty -m "$i on $M $D $Y"
