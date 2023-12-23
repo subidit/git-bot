cd91fde048f386dc11041196da193d469e408cd5
Subidit Win <subidit@users.noreply.github.com>
2023-12-23 20:29:50 +0600

    Cleanup

--- bot.sh
+++ bot.sh
@@ -1,15 +1,36 @@
 #!/usr/bin/bash
 
-START_Y=2012
-END_Y=2013
+# Input start and end dates
+read -p "Enter start date (yyyy-mm-dd): " START_DATE
+read -p "Enter end date (yyyy-mm-dd): " END_DATE
 
-for Y in $(eval "echo {$START_Y..$END_Y}"); do
-    for M in {01..2}; do
-        for D in {01..31}; do
-            for i in {01..12}; do
-                GIT_AUTHOR_DATE="$Y-$M-$D $i:00:00" GIT_COMMITTER_DATE="$Y-$M-$D $i:00:00" git commit --allow-empty -m "$i on $M $D $Y"
-            done
-        done
+# Convert start and end dates to Unix timestamps
+START_UNIX=$(date -d "$START_DATE" +%s)
+END_UNIX=$(date -d "$END_DATE" +%s)
+
+# Loop through the timestamps and perform commits
+while [ "$START_UNIX" -le "$END_UNIX" ]; do
+    # Extract year, month, and day from the current date
+    CURRENT_DATE=$(date -d "@$START_UNIX" "+%Y-%m-%d")
+    YEAR=$(date -d "@$START_UNIX" "+%Y")
+    MONTH=$(date -d "@$START_UNIX" "+%-m")
+    DAY=$(date -d "@$START_UNIX" "+%-d")
+
+    # Generate a random number between 0 and 9 for commit count
+    RANDOM_COUNT=$((RANDOM % 10))
+
+    # Loop to perform random commits
+    for ((i = 0; i < RANDOM_COUNT; i++)); do
+        # Generate a random hour for commit time
+        HOUR=$((RANDOM % 24))
+        MIN=$((RANDOM % 60))
+        SEC=$((RANDOM % 60))
+
+        GIT_AUTHOR_DATE="$YEAR-$(printf %02d $MONTH)-$(printf %02d $DAY) $HOUR:$MIN:$SEC" GIT_COMMITTER_DATE="$YEAR-$(printf %02d $MONTH)-$(printf %02d $DAY) $HOUR:$MIN:$SEC" git commit --allow-empty -m "$i on $(printf %02d $MONTH) $(printf %02d $DAY) $YEAR"
     done
+
+    # Move to the next day
+    START_UNIX=$((START_UNIX + 86400)) # Increment by 24 hours (in seconds)
 done
+
 git push origin master
